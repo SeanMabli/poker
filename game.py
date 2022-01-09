83c79b23d382d27e8d197857d4a635f48506e4a5
SeanMabli <smdrone1@gmail.com>
2022-01-09 07:55:11 -0500

    work on cash moving details & improve terminal ui

--- game.py
+++ game.py
@@ -2,36 +2,34 @@ import numpy as np
 import os
 
 input('use enter to advance through the game')
-os.system('clear')
+os.system('cls')
 
 playernames = np.array(['zain', 'adam']) # np.array(input("enter player names (seprated by comma's): ").strip().lower().split(','))
-os.system('clear')
+os.system('cls')
 
 smallblind = 1 # int(input('small blind: '))
 blind = np.array([smallblind, smallblind * 2])
-os.system('clear')
+os.system('cls')
 
 cash = 200 # int(input('player starting cash: '))
 playercash = np.array([cash] * len(playernames))
-os.system('clear')
+os.system('cls')
 
 playercards = np.empty([len(playernames), 2, 2], str)
 dealercards = np.empty([5, 2], str)
 
-playerbets = np.zeros(playernames.shape, dtype=int)
-playerbets[0], playerbets[1] = blind[0], blind[1]
-playercash -= playerbets
-aliveplayerbets = playerbets
-minbet = blind[1]
-pot = 0
-
-playercombos = np.zeros((len(playernames), 10))
-playerfold = np.full(len(playernames), False, dtype=bool)
-
 gamenum = 0
 while True:
+  
+  playerbets = np.zeros(playernames.shape, dtype=int)
   playerbets[0], playerbets[1] = blind[0], blind[1]
+  aliveplayerbets = playerbets
   playercash -= playerbets
+
+  minbet = blind[1]
+  pot = 0
+
+  playerfold = np.full(len(playernames), False, dtype=bool)
 
   deck = np.array([['A', 'C'], ['2', 'C'], ['3', 'C'], ['4', 'C'], ['5', 'C'], ['6', 'C'], ['7', 'C'],['8', 'C'], ['9', 'C'], ['10', 'C'], ['J', 'C'], ['Q', 'C'], ['K', 'C'], ['A', 'D'], ['2', 'D'], ['3', 'D'], ['4', 'D'], ['5', 'D'], ['6', 'D'], ['7', 'D'],['8', 'D'], ['9', 'D'], ['10', 'D'], ['J', 'D'], ['Q', 'D'], ['K', 'D'], ['A', 'H'], ['2', 'H'], ['3', 'H'], ['4', 'H'], ['5', 'H'], ['6', 'H'], ['7', 'H'],['8', 'H'], ['9', 'H'], ['10', 'H'], ['J', 'H'], ['Q', 'H'], ['K', 'H'], ['A', 'S'], ['2', 'S'], ['3', 'S'], ['4', 'S'], ['5', 'S'], ['6', 'S'], ['7', 'S'],['8', 'S'], ['9', 'S'], ['10', 'S'], ['J', 'S'], ['Q', 'S'], ['K', 'S']])
   np.random.shuffle(deck)
@@ -72,13 +70,10 @@ while True:
             playerbets[i] = minbet
           elif y == 'f' or y == 'fold':
             playerfold[i] = True
-          os.system('clear')
+          os.system('cls')
 
         if np.sum(playerfold) == len(playernames) - 1:
-          os.system('clear')
-          playercash[playerfold.tolist().index(False)] += np.sum(playerbets)
-          playerbets = np.zeros(playernames.shape, dtype=int)
-          playerfold = np.full(len(playernames), True, dtype=bool)
+          os.system('cls')
           break
 
       loop = True
@@ -90,17 +85,23 @@ while True:
           aliveplayerbets = np.delete(aliveplayerbets, i - x)
           x += 1
 
-    pot += np.sum(playerbets)       
+    pot += np.sum(playerbets)
     playerbets = np.zeros(playernames.shape, dtype=int)
   
-  print('dealer cards: ' + dealercards[0, 0] + dealercards[0, 1] + ', ' + dealercards[1, 0] + dealercards[1, 1] + ', ' + dealercards[2, 0] + dealercards[2, 1] + ', ' + dealercards[3, 0] + dealercards[3, 1] + ', ' + dealercards[4, 0] + dealercards[4, 1])
-  for player, cards in enumerate(playercards):
-    print(playernames[player] + "'s cards: " + cards[0, 0] + cards[0, 1] + ', ' + cards[1, 0] + cards[1, 1])
+  if np.sum(playerfold) == len(playernames) - 1:
+    playercash[playerfold.tolist().index(False)] += pot
+  else:
+    print('dealer cards: ' + dealercards[0, 0] + dealercards[0, 1] + ', ' + dealercards[1, 0] + dealercards[1, 1] + ', ' + dealercards[2, 0] + dealercards[2, 1] + ', ' + dealercards[3, 0] + dealercards[3, 1] + ', ' + dealercards[4, 0] + dealercards[4, 1])
+    for player, cards in enumerate(playercards):
+      print(playernames[player] + "'s cards: " + cards[0, 0] + cards[0, 1] + ', ' + cards[1, 0] + cards[1, 1])
 
-  winner = input('who won? ').strip().lower()
+    winner = input('who won? ').strip().lower()
+    for i, player in enumerate(playernames):
+      if winner == player:
+        playercash[i] += pot
+
   for i, player in enumerate(playernames):
-    if winner == player:
-      playercash[i] += pot
-
-  print(playercash)
+    print(player + "'s cash: " + str(playercash[i]))
+  input('press enter to continue to next game')
+  os.system('cls')
   gamenum += 1
