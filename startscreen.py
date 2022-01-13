c412fd2bb80c3d97c64511b1694b0389748e9a6b
SeanMabli <smdrone1@gmail.com>
2022-01-12 21:35:00 -0500

    continue work on start screen

--- startscreen.py
+++ startscreen.py
@@ -1,20 +1,25 @@
 import pygame
+import numpy as np
 
+# pygame init
 pygame.init()
 pygame.display.set_caption('poker')
 
+# pygame variables
 display = pygame.display.set_mode((360, 450))
 playernamesbox = [pygame.Rect(10, i * 40 + 40, 150, 32) for i in range(10)]
-playernames = ['' for _ in range(10)]
 plusbox = pygame.Rect(70, 120, 20, 32)
 blindbox = pygame.Rect(200, 40, 150, 32)
-blind = ''
 startingcashbox = pygame.Rect(200, 160, 150, 32)
-startingcash = ''
-startbox = pygame.Rect(200, 240, 150, 32)
 active = [False for _ in range(12)]
 screen = 'start'
 done = False
+
+# game variables
+playernames = ['' for _ in range(10)]
+blind = ''
+startingcash = ''
+startbox = pygame.Rect(200, 240, 150, 32)
 
 while not done:
   for event in pygame.event.get():
@@ -47,29 +52,39 @@ while not done:
       pygame.draw.rect(display, (255, 255, 255), startbox, 2)
       display.blit(pygame.font.Font(None, 32).render("start", True, (255, 255, 255)), (248, 245))
 
+      # mouse clicks
       if event.type == pygame.MOUSEBUTTONDOWN:
+        # player name boxes
         for x in range(int(plusbox.y / 40 - 1)):
           if playernamesbox[x].collidepoint(event.pos):
             active[x] = not active[x]
           else:
             active[x] = False
 
+        # plus box
         if plusbox.collidepoint(event.pos):
           plusbox.y += 40
-          
+        
+        # blind box
         if blindbox.collidepoint(event.pos):
           active[10] = not active[10]
         else:
           active[10] = False
 
+        # starting cash box
         if startingcashbox.collidepoint(event.pos):
           active[11] = not active[11]
         else:
           active[11] = False
         
+        # start button
         if startbox.collidepoint(event.pos):
-          blind = int(blind)
-          startingcash = int(startingcash)
+          for i in reversed(playernames):
+            if i == '':
+              playernames.pop()
+          playernames = np.array(playernames)
+          playercash = np.array([int(startingcash)] * len(playernames))
+          blind = np.array([int(blind), int(blind) * 2])
           screen = 'player'
 
       for x in range(int(plusbox.y / 40 - 1)):
